1ce359eeb0e461dbdb50cef8a5fd783b6e164fce
Nosa Osagiede <osagiedenosakhare@gmail.com>
2023-04-19 11:53:48 +0100

    add lost in time

--- CastVideos.js
+++ CastVideos.js
@@ -935,11 +935,11 @@ CastPlayer.getErrorMessage = function(error) {
  */
 var mediaJSON = { 'categories' : [{ 'name' : 'Movies',
     'videos' : [
-        { 'description' : "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
-            'sources' : ['http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'],
+        { 'description' : "Lost in time",
+            'sources' : ['https://mymovies-payload-cms.herokuapp.com/trailer/lostintimetrailer.mp4'],
             'subtitle' : 'By Blender Foundation',
             'thumb' : 'images/BigBuckBunny.jpg',
-            'title' : 'Big Buck Bunny'
+            'title' : 'Lost in Time'
         },
         { 'description' : 'The first Blender Open Movie from 2006',
             'sources' : ['http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'],
